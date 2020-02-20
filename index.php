af852152b267fc79b7dc8113ff2711715b5b0a96
newnag <48855584+newnag@users.noreply.github.com>
2020-02-20 17:31:42 +0700

    เริ่มต้น berhoro

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BerHoRo</title>
+
+    <link rel="stylesheet" href="css/berstyle.min.css?v=<?=date('his'); ?>">
+
+    <script src="https://kit.fontawesome.com/37e3c13129.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="plugin/owl.carousel.css">
+    <link rel="stylesheet" href="plugin/owl.theme.default.css">
+</head>
+<body>
+
+    <nav>
+        <div class="topnav">
+            <div class="logo-nav"><figure><a href=""><img src="img/logo/logo.png" alt=""></a></figure></div>
+
+            <ul class="nav-menu">
+                <li><a href="">ทำนายเบอร์</a></li>
+                <li><a href="">ค้นหาเบอร์จากความหมาย</a></li>
+                <li><a href="">บทความ</a></li>
+                <li><a href="">วิธีการสั่งซื้อ</a></li>
+                <li><a href="">เกี่ยวกับเรา</a></li>
+                <li><a href="">ติดต่อเรา</a></li>
+                <li><a href=""><i class="fas fa-shopping-basket"></i></a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="slider owl-carousel">
+        <figure><img src="img/Artboard–30.jpg" alt=""></figure>
+    </div>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="plugin/owl.carousel.js"></script>
+    <script src="js/slider.js?v=<?=date('his'); ?>"></script>
+</body>
+</html>
---
+++ js/slider.js
@@ -0,0 +1,7 @@
+$('.slider').owlCarousel({
+    loop:true,
+    nav:true,
+    item:1,
+    responsiveClass:true,
+    autoWidth:true
+})
